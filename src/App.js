85d6702cdf181a22602f265fbfa4cb7509134e2b
Palo23 <lmarespinortizz23@gmail.com>
2022-03-11 21:39:25 -0600

    feat: Create api functions and main view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
+import { getPeople, getCharacter, searchCharacter } from './api/people';
 import './App.css';
 
 function App() {
+  const inputSearch = useRef(null);
+  const [people, setPeople] = useState([]);
+  const [page, setPage] = useState(1);
+  const [textSearch, setTextSearch] = useState('');
+  const [currentCharacter, setCurrentCharacter] = useState(1);
+  const [details, setDetails] = useState({})
+  const [errorState, setErrorState] = useState({ hasError: false });
+
+  useEffect(() => {
+    getPeople(page).then(setPeople).catch(handleError);
+  }, [page]);
+
+  useEffect(() => {
+    getCharacter(currentCharacter).then(setDetails).catch(handleError)
+  })
+
+  const handleError = (error) => {
+    setErrorState({ hasError: true, message: error.message });
+  }
+
+  const showDetails = (character) => {
+    const id = Number(character.url.split('/').slice(-2)[0]);
+    setCurrentCharacter(id);
+  }
+
+  const onChangeTextSearch = (event) => {
+    event.preventDefault();
+    const text = inputSearch.current.value
+    setTextSearch(text);
+  }
+
+  const onSearchSubmit = (event) => {
+    if (event.key !== 'Enter') return;
+
+    inputSearch.current.value = '';
+    setDetails({});
+    searchCharacter(textSearch).then(setPeople).catch(handleError);
+  }
+
+  const onChangePage = (next) => {
+    if (!people.previus && page + next <= 0) return;
+    if (!people.next && page + next >= 9) return;
+
+    setPage(page + next);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        ref={inputSearch} 
+        onChange={onChangeTextSearch} 
+        onKeyDown={onSearchSubmit}
+        type="text" 
+        placeholder="Enter character" 
+      />
+      <ul>
+      {errorState.hasError && <div>{errorState.message}</div>}
+      {people?.results?.map((character)=> (
+        <li key={character.name} onClick={() => showDetails(character)}>
+          {character.name}
+          </li>
+      ))}
+    </ul>
+
+      <section>
+        <button onClick={() => onChangePage(-1)}>Prev</button>
+        | {page} |
+        <button onClick={() => onChangePage(1)}>Next</button>
+      </section>
+
+    {details && (
+      <aside>
+      <h1>{details.name}</h1>
+      <ul>
+        <li>Height: {details.height}</li>
+        <li>Mass: {details.mass}</li>
+        <li>Year of birth: {details.birth_year}</li>
+      </ul>
+    </aside>
+    )}
     </div>
   );
 }
